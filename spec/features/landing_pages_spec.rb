4cba12724621fa214d04cc20f7f4b4801d7d58d5
Mike Guerrero <mguerrero8816@yahoo.com>
2016-07-05 15:39:20 -0700

    landing page testing

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "LandingPages", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'visiting the website for the first time' do
+    Steps 'visiting the website' do
+      When 'I visit the website for the first time' do
+        visit '/'
+        expect(page).to have_content("Welcome to Paul's Planner")
+        expect(page).to have_content("Mission Statement")
+      end
+    end
+  end
 end
