ba1fbd6e23785eef1eb68c473bf6a6128eaf8bee
Sean Fernandez <scfernz@gmail.com>
2016-07-06 15:33:25 -0700

    Added styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706171954) do
-
+ActiveRecord::Schema.define(version: 20160706172013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
