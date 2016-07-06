712ec78226f7c7dd26298c9948241b510ba4c840
minachuong <mina.chuong@gmail.com>
2016-07-06 10:47:28 -0700

    added bootstrap gems

---
+++ spec/features/navbars_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Navbars", type: :feature do
+  context 'Navigating the website with the navbar' do
+    Steps 'Navigating the website with the navbar' do
+      Given 'I am on the landing page' do
+        visit '/'
+      end
+      Then 'I can see a navbar' do
+        click_link("Paul's Planner")
+        expect(page).to have_content("Mission Statement")
+      end
+    end
+  end
+end
