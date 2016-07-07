20363c08bfc51ec9e4f556468c7ea37ff50d67cc
Fiona Voss <fionavoss@gmail.com>
2016-07-07 14:58:10 -0700

    students can only see their own tasks on their profile page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,11 @@
 class UserController < ApplicationController
+
   def index
+    if current_user.nil?
+      redirect_to '/users/sign_in'
+    else
+      @tasks = Task.where(user: current_user)
+    end
   end
 
   def approve_account
--- spec/features/student_profiles_spec.rb
+++ spec/features/student_profiles_spec.rb
@@ -3,13 +3,47 @@ require 'rails_helper'
 RSpec.feature "StudentProfiles", type: :feature do
   context 'seeing a student profile page' do
     Steps 'seeing a student profile page' do
-      Given 'that I am a student and have logged in' do
-        # visit '/studentlogin'
-        #click button...
-        # click_button('Login')
+      Given 'that I am a student and I have tasks' do
+        first_student = User.new
+        first_student.email = 'studentone@student.com'
+        first_student.password = '123456'
+        first_student.password_confirmation = '123456'
+        first_student.save!
+        first_student.remove_role :provisional
+        first_student.add_role :student
+
+        second_student = User.new
+        second_student.email = 'studenttwo@student.com'
+        second_student.password = '123456'
+        second_student.password_confirmation = '123456'
+        second_student.save!
+        second_student.remove_role :provisional
+        second_student.add_role :student
+
+        student1_task = Task.new
+        student1_task.user_id = first_student.id
+        student1_task.title = "taskone"
+        student1_task.save!
+
+        student2_task = Task.new
+        student2_task.user_id = second_student.id
+        student2_task.title = "tasktwo"
+        student2_task.save!
+      end
+      And 'I have logged in' do
+        visit '/users/sign_in'
+        fill_in "user[email]", with: 'studentone@student.com'
+        fill_in "user[password]", with: "123456"
+        click_button "Log in"
       end
       Then 'I can see my information' do
-        # expect(page).to have_content('Welcome, Joe')
+        expect(page).to have_content 'studentone@student.com'
+      end
+      And 'I can see my tasks' do
+        expect(page).to have_content 'taskone'
+      end
+      And "I can't see other users' tasks" do
+        expect(page).to_not have_content 'tasktwo'
       end
     end
   end
