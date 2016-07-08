76031f7ef34d9317709f8b70cfaa2e9b5982878f
minachuong <mina.chuong@gmail.com>
2016-07-08 14:15:28 -0700

    meeting and login tests working

--- spec/features/meetings_spec.rb
+++ spec/features/meetings_spec.rb
@@ -4,27 +4,27 @@ RSpec.feature "Meetings", type: :feature do
   context "Schedule a meeting with teacher" do
     Steps "Schedule a meeting with teacher" do
       Given "That I am a student and on the meeting page" do
-        # first_student = User.new
-        # first_student.email = 'studentone@student.com'
-        # first_student.password = '123456'
-        # first_student.password_confirmation = '123456'
-        # first_student.save!
-        # first_student.remove_role :provisional
-        # first_student.add_role :student
+        first_student = User.new
+        first_student.email = 'studentone@student.com'
+        first_student.password = '123456'
+        first_student.password_confirmation = '123456'
+        first_student.save!
+        first_student.remove_role :provisional
+        first_student.add_role :student
 
-        # visit "/"
-        # click_link("Students")
-        # fill_in "email", with: 'fmezqlp_carrieroson_1467998722@tfbnw.net'
-        # fill_in "pass", with: 'oxwiqj9fqf4'
-        # click_button("login")
-        # expect(page).to have_content "Student"
+        visit "/"
+        click_link "Login"
+        fill_in "user[email]", with: "studentone@student.com"
+        fill_in "user[password]", with: "123456"
+        click_button "Log in"
+
         click_link('Meetings')
         expect(page).to have_content("Listing Meetings")
       end
       And "have created a meeting" do
         click_link("New Meeting")
-        fill_in "Location", with: "here"
-        fill_in "Description", with: "there"
+        fill_in "meeting[location]", with: "here"
+        fill_in "meeting[description]", with: "there"
         click_button "Create Meeting"
         expect(page).to have_content("Meeting was successfully created")
       end
--- spec/features/student_logins_spec.rb
+++ spec/features/student_logins_spec.rb
@@ -5,10 +5,6 @@ RSpec.feature "StudentLogins", type: :feature do
     Steps "A user logging in as a student" do
       Given "I am a facebook account and am logged in" do
         visit "/"
-        click_link("Students")
-        fill_in "email", with: 'fmezqlp_carrieroson_1467998722@tfbnw.net'
-        fill_in "pass", with: 'oxwiqj9fqf4'
-        click_button("loginbutton")
       end
       Then "I can see my student profile" do
         expect(page).to have_content "Student"
