fbf837509cf2a7429e0ec01c0b5234952d9f60cd
minachuong <mina.chuong@gmail.com>
2016-07-08 13:50:00 -0700

    added test for student login

--- spec/features/meetings_spec.rb
+++ spec/features/meetings_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
-RSpec.feature "Meetings", type: :feature do
+RSpec.feature "Meetings", type: :feature js: true do
   context "Schedule a meeting with teacher" do
     Steps "Schedule a meeting with teacher" do
       Given "That I am a student and on the meeting page" do
-        #log in via social media...eventually
-        first_student = User.new
-        first_student.email = 'studentone@student.com'
-        first_student.password = '123456'
-        first_student.password_confirmation = '123456'
-        first_student.save!
-        first_student.remove_role :provisional
-        first_student.add_role :student
-        visit '/meetings'
+        # first_student = User.new
+        # first_student.email = 'studentone@student.com'
+        # first_student.password = '123456'
+        # first_student.password_confirmation = '123456'
+        # first_student.save!
+        # first_student.remove_role :provisional
+        # first_student.add_role :student
+
+        # visit "/"
+        # click_link("Students")
+        # fill_in "email", with: 'fmezqlp_carrieroson_1467998722@tfbnw.net'
+        # fill_in "pass", with: 'oxwiqj9fqf4'
+        # click_button("login")
+        # expect(page).to have_content "Student"
+        click_link('Meetings')
         expect(page).to have_content("Listing Meetings")
       end
       And "have created a meeting" do
