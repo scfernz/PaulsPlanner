5b2d2678717ed57943f24158b7b13ce108c9a443
Joshua Lapid <lapidjoshua@yahoo.com>
2016-07-12 13:50:18 -0700

    Can change profile picture through edit page

--- spec/features/picture_uploads_spec.rb
+++ spec/features/picture_uploads_spec.rb
@@ -1,20 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "PictureUploads", type: :feature do
+RSpec.feature "PictureUploads", type: :feature, js: true do
   context 'seeing a student profile page' do
     Steps 'seeing a student profile page' do
-      Given 'that I am a student' do
-        first_student = User.new
-        first_student.email = 'studentone@student.com'
-        first_student.password = '123456'
-        first_student.password_confirmation = '123456'
-        first_student.save!
-        first_student.remove_role :provisional
-        first_student.add_role :student
-      end
-      And 'I have logged in' do
+      Given 'I have logged in' do
         visit '/users/sign_in'
-        fill_in "user[email]", with: 'studentone@student.com'
+        fill_in "user[email]", with: 'student1@student.com'
         fill_in "user[password]", with: "123456"
         click_button "Log in"
       end
@@ -22,12 +13,15 @@ RSpec.feature "PictureUploads", type: :feature do
         expect(page).to have_xpath("//img[@src=\"/assets/default-profile-e08597880fc222202f22984a4f1966a29b108e856a3fb935072bfbbc302a4b73.png\"]")
       end
       Then 'I can upload a picture' do
+        click_link 'Edit Profile'
+        fill_in "user[current_password]", with: "123456"
         attach_file('user[image]', Rails.root + 'public/apple-icon.png')
-        click_button 'Update User'
+        click_button 'Update'
+        save_and_open_page
       end
       And 'I can see the image on my profile' do
         visit '/'
-        page.should have_xpath("//img[@alt='Apple icon']")
+        expect(page).to have_xpath("//img[@alt='Apple icon']")
       end
     end
   end
