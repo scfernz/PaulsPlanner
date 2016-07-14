80acd5a5899426e30dc5a02884b0c6be0d8a054d
Mina Chuong <mina.chuong@gmail.com>
2016-07-13 17:11:10 -0700

    added students index page to view all students

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
 
   get 'provisional/index'
 
+  get 'students/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
