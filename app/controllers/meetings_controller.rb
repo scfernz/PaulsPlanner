90e4ddf4c29b02c942f7e08d97b86c9b754832cd
Mina Chuong <mina.chuong@gmail.com>
2016-07-15 13:22:24 -0700

    names link to profiles

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -23,6 +23,7 @@ class MeetingsController < ApplicationController
   # GET /meetings/1
   # GET /meetings/1.json
   def show
+    @user_createdby = User.find(@meeting.created_by)
   end
 
   # GET /meetings/new
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,7 @@ class TasksController < ApplicationController
   # GET /tasks/1
   # GET /tasks/1.json
   def show
+    @user_createdby = User.find(@task.createdby_id)
   end
 
   # GET /tasks/new
