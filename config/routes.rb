260af92172589330309878f327e19a4bd5a3145a
ramin Mesghali <ramesghali@gmail.com>
2016-07-18 13:49:20 -0700

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,8 @@ Rails.application.routes.draw do
 
   get 'students/index'
 
+  get 'landing/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
