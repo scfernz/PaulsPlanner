6227a645ef8f3012622f0e8524f0777cd94e2df9
Joshua Lapid <lapidjoshua@yahoo.com>
2016-07-19 14:43:34 -0700

    Changed true/false to yes/no in task show

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -75,7 +75,7 @@ RSpec.feature "Tasks", type: :feature do
       end
       And "The task page shows that the task has been Turn In and will also not have a 'Turn In' button" do
         click_link "taskone"
-        expect(page).to have_content "false"
+        expect(page).to have_content "No"
         expect{click_button "Mark Incomplete"}.to raise_error('Unable to find button "Mark Incomplete"')
       end
       And "I can log in as a teacher and see the status of the task" do
@@ -99,13 +99,13 @@ RSpec.feature "Tasks", type: :feature do
       Then "I can Turn In that task by clicking on a button on the task page" do
         click_link "taskone"
         click_button "Mark Complete"
-        expect(page).to have_content "true"
+        expect(page).to have_content "Yes"
       end
       And 'The Turn In button is replaced by a Mark Incomplete button' do
         click_button 'Mark Incomplete'
       end
       When 'I user the Mark Incomplete button, the task is considered incomplete' do
-        expect(page).to have_content 'false'
+        expect(page).to have_content 'No'
       end
     end
 
