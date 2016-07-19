e2afaa29ec34528701d8cf4de7ae020777d81a2a
Ramin Mesghali <ramesghali@gmail.com>
2016-07-19 10:32:46 -0700

    Update profile button changed to Apply Changes

--- spec/features/picture_uploads_spec.rb
+++ spec/features/picture_uploads_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature "PictureUploads", type: :feature do
         click_link 'Edit Profile'
         fill_in "user[current_password]", with: "123456"
         attach_file('user[image]', Rails.root + 'public/apple-icon.png')
-        click_button 'Update'
+        click_button 'Apply Changes'
       end
       And 'I can see the image on my profile' do
         visit '/'
